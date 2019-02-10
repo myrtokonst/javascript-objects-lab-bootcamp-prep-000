520418a11278eb6e717acc1d387de7ba11a6864b
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-02-10 21:27:14 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,8 @@
+var recipes = new Object ({})
+var obj = { prop: 1 }
+function updateObjectWithKeyAndValue(object, key, value) { return Object.assign({},obj, {[key]:value})}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) { object[key]= value
+  return object
+}
+var obj2 = Object.assign({}, obj)
+function deleteFromObjectByKey(object, key) { re
